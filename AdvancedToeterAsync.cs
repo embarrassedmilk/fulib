dd9002acb77c6da684945ba46be674a5fd3359c3
Valerii <akai.no.ishi@gmail.com>
2017-12-30 10:40:37 +0100

    fix async

--- AdvancedToeterAsync.cs
+++ AdvancedToeterAsync.cs
@@ -4,19 +4,22 @@ using LanguageExt;
 
 namespace func {
     public class LocationRepositoryAsync {
-        public Option<Location> Get(int id) {
+        public async Task<Option<Location>> Get(int id) {
+            await Task.CompletedTask;
             return id % 2 == 0 ? Option<Location>.Some(new Location(id)) : Option<Location>.None;
         }
     }
 
     public class ClientRepositoryAsync {
-        public Option<Client> Get(int id) {
+        public async Task<Option<Client>> Get(int id) {
+            await Task.CompletedTask;
             return id % 2 == 0 ? Option<Client>.Some(new Client(id)) : Option<Client>.None;
         }
     }
 
     public class ItemRepositoryAsync {
-        public Option<Item> Get(int id) {
+        public async Task<Option<Item>> Get(int id) {
+            await Task.CompletedTask;
             return id % 2 == 0 ? Option<Item>.Some(new Item(id)) : Option<Item>.None;
         }
     }
@@ -26,20 +29,14 @@ namespace func {
         private readonly ClientRepositoryAsync _clientRepository = new ClientRepositoryAsync();
         private readonly ItemRepositoryAsync _itemRepository = new ItemRepositoryAsync();
 
-        public void SendEmail(int locationId, int clientId, int itemId) {
+        public async Task SendEmail(int locationId, int clientId, int itemId) {
             
             var letter = _locationRepository.Get(locationId)
-                .Bind(location => _clientRepository.Get(clientId)
-                .Bind(client => _itemRepository.Get(itemId)
-                .Bind(item => CreateActualEmail(location, client, item))));
-            
-            // var letter = 
-            //     from location in _locationRepository.Get(locationId)
-            //     from client in _clientRepository.Get(clientId)
-            //     from item in _itemRepository.Get(itemId)
-            //     select CreateActualEmail(location, client, item);
+                .BindAsync(loc =>  _clientRepository.Get(clientId)
+                .BindAsync(cl => _itemRepository.Get(itemId)
+                .BindAsync(item => CreateActualEmail(loc, cl, item))));
 
-            letter.Match(
+            await letter.Match(
                 Some: val => Console.WriteLine(val),
                 None: () => Console.WriteLine("Smth went wrong...")
             );
