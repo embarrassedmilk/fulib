5c964fbfa3f673d842d4a31a77bb498fe8c45662
Valerii <akai.no.ishi@gmail.com>
2018-01-08 21:42:25 +0100

    add task extensions

--- src/ListExtensions.cs
+++ src/ListExtensions.cs
@@ -1,5 +1,6 @@
 using LanguageExt;
 using System;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 
 namespace func {
@@ -10,7 +11,7 @@ namespace func {
             return list;
         }
 
-        public static Result<List<B>> TraverseResultAppl<A,B>(Func<A, Result<B>> f, List<A> list) {
+        public static Result<List<B>> TraverseResultAppl<A,B>(this List<A> list, Func<A, Result<B>> f) {
             var initialState = Result.Success(new List<B>());
 
             Func<Result<List<B>>, A, Result<List<B>>> folder = (state, item) => {
@@ -23,11 +24,27 @@ namespace func {
             return List.foldBack<Result<List<B>>, A>(list, initialState, folder);
         }
 
-        public static Result<List<A>> SequenceResultAppl<A>(List<Result<A>> list) {
+        public static Result<List<A>> SequenceResultAppl<A>(this List<Result<A>> list) {
             Func<Result<A>, Result<A>> id = (r) => new Identity<Result<A>>(r).Value;
-            return TraverseResultAppl(id, list);
+            return list.TraverseResultAppl(id);
         }
 
-        
+        public static Task<List<B>> TraverseTaskAppl<A,B>(this List<A> list, Func<A, Task<B>> f) {
+            var initialState = TaskExtensions.AsTask(new List<B>()); //AsTask - from l-ext
+
+            Func<Task<List<B>>, A, Task<List<B>>> folder = (state, item) => {
+                Func<B,List<B>,List<B>> prependFunc = Prepend;
+                var cons = TaskExtensions.AsTask(prependFunc.Curry().Flip()); //AsTask - from l-ext
+                
+                return f(item).Apply(state.Apply(cons));
+            };
+
+            return List.foldBack<Task<List<B>>, A>(list, initialState, folder);
+        }
+
+        public static Task<List<A>> SequenceTaskAppl<A,B>(List<Task<A>> list) {
+            Func<Task<A>, Task<A>> id = (t) => new Identity<Task<A>>(t).Value;
+            return list.TraverseTaskAppl(id);
+        }
     }
 }
---
+++ src/TaskExtensions.cs
@@ -0,0 +1,14 @@
+using LanguageExt;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace func {
+    public static class AsyncTaskExtensions {
+        public async static Task<B> Apply<A,B>(this Task<A> task, Task<Func<A,B>> f) {
+            var taskResult = await task;
+            var fResult = await f;
+            return fResult(taskResult);
+        }
+    }
+}
