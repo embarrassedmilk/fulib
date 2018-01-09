a84494ed6be61725770651dd4fba0f871a49661c
Valerii <akai.no.ishi@gmail.com>
2018-01-09 21:56:52 +0100

    use of applicative traverse

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace func
 {
@@ -6,6 +7,22 @@ namespace func
     {
         static void Main(string[] args)
         {
+            var goodList = new List<string>() {
+                "http://google.com",
+                "http://bbc.co.uk",
+                "http://fsharp.org",
+                "http://fsharpforfunandprofit.com"
+            };
+
+            var result = Traverser.GetMaxLengthOfWebsitesContentA(goodList).Result;
+
+            if (result.IsSuccess) {
+                Console.WriteLine($"Max content size is {result.Value}");
+            }
+            else {
+                Console.WriteLine("Errors: ");
+                Console.WriteLine(string.Join(" | ", result.Errors));
+            }
         }
     }
 }
